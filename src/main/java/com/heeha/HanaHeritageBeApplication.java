9691fb3ac7b8444927dd43dfef44a47feed6a093
hyerimmy <hyerim2000@swu.ac.kr>
2024-05-23 10:17:47 +0900

    feat : 초기 환경 셋팅

---
+++ src/main/java/com/heeha/HanaHeritageBeApplication.java
@@ -0,0 +1,13 @@
+package com.heeha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HanaHeritageBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HanaHeritageBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/heeha/HanaHeritageBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.heeha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HanaHeritageBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
