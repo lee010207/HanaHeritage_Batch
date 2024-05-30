0a339d7dea4e19a6c66988b504fa2d2d2d2869a9
hyerimmy <hyerim2000@swu.ac.kr>
2024-05-30 11:31:44 +0900

    chore : History Entity 수정 - 지후 요청

--- src/main/java/com/heeha/domain/history/entity/History.java
+++ src/main/java/com/heeha/domain/history/entity/History.java
@@ -4,8 +4,10 @@ import com.heeha.domain.account.entity.Account;
 import com.heeha.domain.base.entity.BaseEntity;
 import jakarta.persistence.*;
 import lombok.*;
+import org.hibernate.annotations.ColumnDefault;
+import org.springframework.data.annotation.CreatedDate;
 
-import java.sql.Date;
+import java.sql.Timestamp;
 
 @Entity(name = "history")
 @Table(name = "history")
@@ -19,18 +21,35 @@ public class History extends BaseEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    // 출금계좌번호(id기준 조회)
     @ManyToOne
     @JoinColumn(name = "account_id")
     private Account account;
 
-    private Date dealdate;
+    @CreatedDate
+    private Timestamp dealdate;
 
+    @ColumnDefault("이체")
     private String dealClassification;
 
+    // 입금액 : 받는 사람 계좌 +
     private int deposit;
 
+    // 출금액 : 보내는 사람 계좌 -
     private int withdraw;
+
+    // 받는 사람 이름
     private String recipient;
+    // 받는 사람 은행
+    private String recipientBank;
+    // 받는 사람 계좌 번호
+    private Long recipientNumber;
+
+    // 보내는 사람
     private String sender;
-    private String remarks;
+
+    // 받는 분 표기
+    private String recipientRemarks;
+    // 보내는 분 표기
+    private String senderRemarks;
 }
