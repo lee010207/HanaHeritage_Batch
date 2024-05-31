bc61d8371ee2253801a7ef75d3649c858359e71f
JeongHeum Byun <quswjdgma83@gmail.com>
2024-05-31 14:32:03 +0900

    feat: Swagger 로그인, 로그아웃 검증위해 주석 해제

--- src/main/java/com/heeha/global/config/SwaggerConfig.java
+++ src/main/java/com/heeha/global/config/SwaggerConfig.java
@@ -1,27 +1,33 @@
 package com.heeha.global.config;
 
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import lombok.RequiredArgsConstructor;
 import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.Arrays;
 
 
 @RequiredArgsConstructor
 @Configuration
 public class SwaggerConfig {
 
-//    @Bean
-//    public OpenAPI openAPI(){
-//        SecurityScheme securityScheme = new SecurityScheme()
-//                .type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT")
-//                .in(SecurityScheme.In.HEADER).name("Authorization");
-//        SecurityRequirement securityRequirement = new SecurityRequirement().addList("bearerAuth");
-//
-//        return new OpenAPI()
-//                .components(new Components().addSecuritySchemes("bearerAuth", securityScheme))
-//                .security(Arrays.asList(securityRequirement));
-//    }
+    @Bean
+    public OpenAPI openAPI(){
+        SecurityScheme securityScheme = new SecurityScheme()
+                .type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT")
+                .in(SecurityScheme.In.HEADER).name("Authorization");
+        SecurityRequirement securityRequirement = new SecurityRequirement().addList("bearerAuth");
+
+        return new OpenAPI()
+                .components(new Components().addSecuritySchemes("bearerAuth", securityScheme))
+                .security(Arrays.asList(securityRequirement));
+    }
 
     @Bean
     public GroupedOpenApi chatOpenApi() {
