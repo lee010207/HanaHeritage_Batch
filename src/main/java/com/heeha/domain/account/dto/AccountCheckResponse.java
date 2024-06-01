8bbbbaa00f41ae7ef9c4af8ffc850b7525298f70
JeongHeum Byun <quswjdgma83@gmail.com>
2024-06-01 16:51:32 +0900

    feat: 고객이 가지고있는 계좌정보 조회위해 Account에 코드 추가

--- src/main/java/com/heeha/domain/account/dto/AccountCheckResponse.java
+++ src/main/java/com/heeha/domain/account/dto/AccountCheckResponse.java
@@ -2,17 +2,23 @@ package com.heeha.domain.account.dto;
 
 import com.heeha.domain.account.entity.Account;
 import java.time.LocalDateTime;
+import java.util.List;
+
+import com.heeha.domain.history.dto.HistoryDto;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.Setter;
 
 @AllArgsConstructor
 @Getter
+@Setter
 public class AccountCheckResponse {
     private Long id;
     private Long accountNumber;
     private String name;
     private Long balance;
     private LocalDateTime createdAt;
+    private List<HistoryDto> histories;
 
     public AccountCheckResponse(Account account) {
         this.id = account.getId();
