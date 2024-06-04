32731e211cba07476e6fac672d31feee8bc418d1
hyerimmy <hyerim2000@swu.ac.kr>
2024-06-05 02:03:33 +0900

    feat : batch 초기 셋팅 및 기본 코드 추가

---
+++ src/main/java/com/heeha/global/job/TestJobConfig.java
@@ -0,0 +1,43 @@
+package com.heeha.global.job;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.DuplicateJobException;
+import org.springframework.batch.core.configuration.support.DefaultBatchConfiguration;
+import org.springframework.batch.core.job.builder.JobBuilder;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.core.step.builder.StepBuilder;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.PlatformTransactionManager;
+
+@Configuration
+@RequiredArgsConstructor
+public class TestJobConfig extends DefaultBatchConfiguration {
+
+    @Bean
+    public Job testJob(JobRepository jobRepository,PlatformTransactionManager transactionManager) throws DuplicateJobException {
+        Job job = new JobBuilder("testJob",jobRepository)
+                .start(testStep(jobRepository,transactionManager))
+                .build();
+        return job;
+    }
+
+    public Step testStep(JobRepository jobRepository,PlatformTransactionManager transactionManager){
+        Step step = new StepBuilder("testStep",jobRepository)
+                .tasklet(testTasklet(),transactionManager)
+                .build();
+        return step;
+    }
+
+    public Tasklet testTasklet(){
+        return ((contribution, chunkContext) -> {
+            System.out.println("***** hello batch! *****");
+            // 원하는 비지니스 로직 작성
+            return RepeatStatus.FINISHED;
+        });
+    }
+}
