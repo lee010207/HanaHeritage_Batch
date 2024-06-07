6517f2066c1723aa339d895905e3d34ab79a702a
hyerimmy <hyerim2000@swu.ac.kr>
2024-06-08 01:12:59 +0900

    fix : 예적금 가입 중 발생 오류 트러블슈팅

--- src/main/java/com/heeha/domain/account/dto/AccountValidationRequest.java
+++ src/main/java/com/heeha/domain/account/dto/AccountValidationRequest.java
@@ -2,8 +2,10 @@ package com.heeha.domain.account.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @AllArgsConstructor
+@NoArgsConstructor
 @Getter
 public class AccountValidationRequest {
     private Long accountId;
